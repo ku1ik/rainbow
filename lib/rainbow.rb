cc3996163d6ac5421b0aa4a2d3873f57eb995f0b
Sebastian Schuberth <sschuberth@gmail.com>
2016-05-20 12:51:11 +0200

    Improve the checks for the dependency on win32console
    This is only required for Ruby < 2.0. Also recognize Cygwin as Windows.
    
    Closes #19 for good :-)

--- lib/rainbow.rb
+++ lib/rainbow.rb
@@ -13,8 +13,7 @@ module Rainbow
 
   # On Windows systems, try to load the local ANSI support library if Ruby version < 2
   # Ruby 2.x on Windows includes ANSI support. 
-  require 'rbconfig'
-  if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/  && RUBY_VERSION.to_i < 2
+  if RUBY_PLATFORM =~ /mswin|cygwin|mingw/ && RUBY_VERSION.to_i < 2
     begin
       require 'Win32/Console/ANSI'
     rescue LoadError
