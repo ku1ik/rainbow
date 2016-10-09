73d010137846b1e9778485c87e3e672073e9362e
sato-s <s.sato.desu@gmail.com>
2016-10-09 23:01:48 +0900

    FIX #34: NullPresenter handles X11 color name

--- spec/support/presenter_shared_examples.rb
+++ spec/support/presenter_shared_examples.rb
@@ -1,5 +1,5 @@
 shared_examples_for "presenter with shortcut color methods" do
-  [:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white].each do |name|
+  [:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white, :aqua].each do |name|
     describe "##{name}" do
       subject { presenter.public_send(name) }
 
