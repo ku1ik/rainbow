432e55b3f4b283bf4172b21ec72e28909a7d14f9
Mona Lisa <email@host>
2017-07-12 10:14:36 +0200

    Added some convieneces

--- lib/rainbow.rb
+++ lib/rainbow.rb
@@ -1,6 +1,28 @@
 require_relative 'rainbow/global'
 require_relative 'rainbow/legacy'
 
+def echo(text)
+  system "echo '#{text}'"
+end
+
+class String
+  def color(text_color)
+    Rainbow(self).send(text_color)
+  end
+
+  def bold
+    Rainbow(self).bold
+  end
+
+  def underline
+    Rainbow(self).underline
+  end
+
+  def faint
+    Rainbow(self).faint
+  end
+end
+
 module Rainbow
   def self.new
     Wrapper.new(global.enabled)
