4e2e86d0b953d91b072d2c3b36872cdb82c1f5d5
David Vargas <dvarrui@protonmail.com>
2022-01-30 10:22:36 +0000

    U adding method Rainbow.available_colours

--- lib/rainbow.rb
+++ lib/rainbow.rb
@@ -7,6 +7,11 @@ module Rainbow
     Wrapper.new(global.enabled)
   end
 
+  def self.available_colours
+    # Using Rainbow... we need to know what colors are available and how are defined
+    X11ColorNames::NAMES
+  end
+
   self.enabled = false unless STDOUT.tty? && STDERR.tty?
   self.enabled = false if ENV['TERM'] == 'dumb'
   self.enabled = true if ENV['CLICOLOR_FORCE'] == '1'
